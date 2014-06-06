70a6fc2aee8144cf80c6c4ffe5e8e4e1ef841ea4
Kostya Vasilyev <kmansoft@gmail.com>
2014-06-06 14:34:13 +0400

    Initial import of Mark Murphy's SetWindow test

---
+++ SetWindowTest/src/com/commonsware/android/wakesvc/PollReceiver.java
@@ -0,0 +1,59 @@
+/***
+  Copyright (c) 2012-2014 CommonsWare, LLC
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+  by applicable law or agreed to in writing, software distributed under the
+  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+  OF ANY KIND, either express or implied. See the License for the specific
+  language governing permissions and limitations under the License.
+  
+  From _The Busy Coder's Guide to Android Development_
+    http://commonsware.com/Android
+ */
+
+package com.commonsware.android.wakesvc;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import java.util.Calendar;
+import com.commonsware.cwac.wakeful.WakefulIntentService;
+
+public class PollReceiver extends BroadcastReceiver {
+  @Override
+  public void onReceive(Context ctxt, Intent i) {
+    if (i.getAction() == null) {
+      WakefulIntentService.sendWakefulWork(ctxt, ScheduledService.class);
+    }
+    else {
+      scheduleAlarms(ctxt);
+    }
+  }
+
+  static void scheduleAlarms(Context ctxt) {
+    AlarmManager mgr=
+        (AlarmManager)ctxt.getSystemService(Context.ALARM_SERVICE);
+    Intent i=new Intent(ctxt, PollReceiver.class);
+    PendingIntent pi=PendingIntent.getBroadcast(ctxt, 0, i, 0);
+
+    Calendar when=Calendar.getInstance();
+
+    when.add(Calendar.MINUTE, 1);
+
+    int unroundedMinutes=when.get(Calendar.MINUTE);
+    int mod=unroundedMinutes % 15;
+
+    when.add(Calendar.MINUTE, 15 - mod);
+    when.set(Calendar.SECOND, 0);
+    when.set(Calendar.MILLISECOND, 0);
+
+    Log.d("PollReceiver", "Scheduling for " + when.toString());
+
+    mgr.setWindow(AlarmManager.RTC_WAKEUP, when.getTimeInMillis(),
+                  30000, pi);
+  }
+}
